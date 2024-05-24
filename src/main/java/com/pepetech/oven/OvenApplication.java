c87ceea37915d384beae371c12139aef6548f265
Edgar Jose Garcia Landaeta <edgar.jose.garcia.l@gmail.com>
2024-05-24 12:18:03 -0500

    refactor

--- src/main/java/com/pepetech/oven/OvenApplication.java
+++ src/main/java/com/pepetech/oven/OvenApplication.java
@@ -11,5 +11,4 @@ public class OvenApplication {
     public static void main(String[] args) {
         SpringApplication.run(OvenApplication.class, args);
     }
-
 }
--- src/main/java/com/pepetech/oven/entities/DataOven.java
+++ src/main/java/com/pepetech/oven/entities/DataOven.java
@@ -1,5 +1,6 @@
 package com.pepetech.oven.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -23,5 +24,6 @@ public class DataOven {
     private Double forecastTemperature;
     private Double error;
     private int strategySelected;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC")
     private Date date = new Date();
 }
